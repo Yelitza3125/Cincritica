4564fa03bc77519af161c665dd71c66c01c1be30
brendamesias <brendamesiasvillar@gmail.com>
2018-01-18 16:00:32 -0500

    trayendo cambios de la rama avance3 de araceli (intereses, estrtura, estilos y funcionalidad)

---
+++ js/intereses.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  var $interestsOption1 = $('#option-1');
+  var $interestsOptions = document.querySelectorAll('.interests-div');
+  var $selection = $('.interests-div');
+  var $genreObject = {genre: []};
+  var $nextButton = $('#next-button');
+  $selection.one('click', function() {
+    var $thisOption = $(this).data('genre');
+    $(this).addClass('interest-selected');
+    console.log($thisOption);
+    var $array = $genreObject.genre;
+    $array.push($thisOption);
+    console.log($genreObject);
+    $nextButton.removeAttr('disabled');
+  });
+});
+
+/*
+ $selection.one('dblclick', function() {
+    debugger;
+    var $thisOption = $(this).data('genre');
+    $(this).removeClass('interest-selected');
+    var $array = $genreObject.genre;
+    $array.indexOf($thisOption);
+    console.log($thisOption);
+  });
+*/ 
--- js/slidenav.js
+++ js/slidenav.js
@@ -9,7 +9,7 @@ firebase.auth()
     saveDataUser(user);
     console.log(user.displayName);
     console.log(user.photoURL);
-
+    
     // Extraemos los datos de usuario para el modal de registro exitoso
     $('#your-photo').append('<img class="img-responsive" src="' + user.photoURL + '"/>');
   });
