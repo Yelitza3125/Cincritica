efdb5603a4d3c8278344228bd458623d7db5c368
Yelitza3125 <deyel_99@hotmail.com>
2018-01-18 15:40:46 -0500

    Redirección de la vista slider

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-$('.carousel.carousel-slider').carousel({fullWidth: true});
+$(document).ready(function() {
+  $('.slider').slider({
+    interval: 3000,
+    transition: 500,
+  });
+
+  setTimeout(function() {
+    window.location.href = 'views/home.html';
+  }, 10000);
+});
+
+
